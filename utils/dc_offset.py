8814b4b333006a3f872dfb5762490d6c80a0a0b1
Bastian Bloessl <bloessl@ccs-labs.org>
2015-04-21 09:05:55 +0200

    dc_offset script change dummy parameters to avoid warnings

--- utils/dc_offset.py
+++ utils/dc_offset.py
@@ -27,7 +27,7 @@ class dc_offset(gr.top_block):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 1000000
 
         ##################################################
         # Blocks
@@ -40,7 +40,7 @@ class dc_offset(gr.top_block):
         	),
         )
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(0, 0)
+        self.uhd_usrp_sink_0.set_center_freq(2.41e9, 0)
         self.uhd_usrp_sink_0.set_gain(0, 0)
         self.uhd_usrp_sink_0.set_dc_offset(bool(dc), 0)
         self.blocks_null_source_0 = blocks.null_source(gr.sizeof_gr_complex*1)
